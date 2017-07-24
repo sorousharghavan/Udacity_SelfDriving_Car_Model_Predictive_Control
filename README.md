d50988916436c4648edafccde0a2481439a47547
sorousharghavan <soroush.arghavan@gmail.com>
2017-07-24 14:10:30 -0400

    initial

---
+++ README.md
@@ -0,0 +1,69 @@
+# CarND-Controls-MPC
+Self-Driving Car Engineer Nanodegree Program
+
+---
+
+_Build instructions at the bottom_
+
+# Car is now able to reach 101 MPH!
+
+## Lap Stats
+_Lap time: 31.4 s _
+_Top speed: 101.06 MPH_
+
+![Race Lap](./lap.gif)
+
+## Dependencies
+
+* cmake >= 3.5
+ * All OSes: [click here for installation instructions](https://cmake.org/install/)
+* make >= 4.1
+  * Linux: make is installed by default on most Linux distros
+  * Mac: [install Xcode command line tools to get make](https://developer.apple.com/xcode/features/)
+  * Windows: [Click here for installation instructions](http://gnuwin32.sourceforge.net/packages/make.htm)
+* gcc/g++ >= 5.4
+  * Linux: gcc / g++ is installed by default on most Linux distros
+  * Mac: same deal as make - [install Xcode command line tools]((https://developer.apple.com/xcode/features/)
+  * Windows: recommend using [MinGW](http://www.mingw.org/)
+* [uWebSockets](https://github.com/uWebSockets/uWebSockets)
+  * Run either `install-mac.sh` or `install-ubuntu.sh`.
+  * If you install from source, checkout to commit `e94b6e1`, i.e.
+    ```
+    git clone https://github.com/uWebSockets/uWebSockets 
+    cd uWebSockets
+    git checkout e94b6e1
+    ```
+* Fortran Compiler
+  * Mac: `brew install gcc` (might not be required)
+  * Linux: `sudo apt-get install gfortran`. Additionall you have also have to install gcc and g++, `sudo apt-get install gcc g++`. Look in [this Dockerfile](https://github.com/udacity/CarND-MPC-Quizzes/blob/master/Dockerfile) for more info.
+* [Ipopt](https://projects.coin-or.org/Ipopt)
+  * Mac: `brew install ipopt`
+       +  Some Mac users have experienced the following error:
+       ```
+       Listening to port 4567
+       Connected!!!
+       mpc(4561,0x7ffff1eed3c0) malloc: *** error for object 0x7f911e007600: incorrect checksum for freed object
+       - object was probably modified after being freed.
+       *** set a breakpoint in malloc_error_break to debug
+       ```
+       This error has been resolved by updrading ipopt with
+       ```brew upgrade ipopt --with-openblas```
+       per this [forum post](https://discussions.udacity.com/t/incorrect-checksum-for-freed-object/313433/19).
+  * Linux
+    * You will need a version of Ipopt 3.12.1 or higher. The version available through `apt-get` is 3.11.x. If you can get that version to work great but if not there's a script `install_ipopt.sh` that will install Ipopt. You just need to download the source from the Ipopt [releases page](https://www.coin-or.org/download/source/Ipopt/) or the [Github releases](https://github.com/coin-or/Ipopt/releases) page.
+    * Then call `install_ipopt.sh` with the source directory as the first argument, ex: `sudo bash install_ipopt.sh Ipopt-3.12.1`. 
+  * Windows: TODO. If you can use the Linux subsystem and follow the Linux instructions.
+* [CppAD](https://www.coin-or.org/CppAD/)
+  * Mac: `brew install cppad`
+  * Linux `sudo apt-get install cppad` or equivalent.
+  * Windows: TODO. If you can use the Linux subsystem and follow the Linux instructions.
+* [Eigen](http://eigen.tuxfamily.org/index.php?title=Main_Page). This is already part of the repo so you shouldn't have to worry about it.
+* Simulator. You can download these from the [releases tab](https://github.com/udacity/self-driving-car-sim/releases).
+* Not a dependency but read the [DATA.md](./DATA.md) for a description of the data sent back from the simulator.
+
+## Basic Build Instructions
+
+1. Clone this repo.
+2. Make a build directory: `mkdir build && cd build`
+3. Compile: `cmake .. && make`
+4. Run it: `./mpc`.
---
+++ src/MPC.cpp
@@ -0,0 +1,284 @@
+#include "MPC.h"
+#include <cppad/cppad.hpp>
+#include <cppad/ipopt/solve.hpp>
+#include "Eigen-3.3/Eigen/Core"
+
+using CppAD::AD;
+
+//higher N adds instability at high speeds due to lag
+size_t N = 12;
+//lower dt adds instability at low speeds
+double dt = 0.07;
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+const double Lf = 2.67;
+
+const size_t x_start = 0;
+const size_t y_start = x_start + N;
+const size_t psi_start = y_start + N;
+const size_t v_start = psi_start + N;
+const size_t cte_start = v_start + N;
+const size_t epsi_start = cte_start + N;
+const size_t delta_start = epsi_start + N;
+const size_t a_start = delta_start + N - 1;
+
+//set point for speed
+const int ref_v = 150;
+
+class FG_eval {
+ public:
+  // Fitted polynomial coefficients
+  Eigen::VectorXd coeffs;
+  FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
+
+  typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
+  void operator()(ADvector& fg, const ADvector& vars) {
+    // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
+    // NOTE: You'll probably go back and forth between this function and
+    // the Solver function below.
+    // The part of the cost based on the reference state.
+
+    //initialize cost
+    fg[0] = 0;
+
+    //initialize
+    fg[1 + x_start] = vars[x_start];
+    fg[1 + y_start] = vars[y_start];
+    fg[1 + psi_start] = vars[psi_start];
+    fg[1 + v_start] = vars[v_start];
+    fg[1 + cte_start] = vars[cte_start];
+    fg[1 + epsi_start] = vars[epsi_start];
+
+    for (unsigned int t = 1; t < N; t++) {
+      // The state at time t+1 .
+      AD<double> x1 = vars[x_start + t];
+      AD<double> y1 = vars[y_start + t];
+      AD<double> psi1 = vars[psi_start + t];
+      AD<double> v1 = vars[v_start + t];
+      AD<double> cte1 = vars[cte_start + t];
+      AD<double> epsi1 = vars[epsi_start + t];
+
+      // The state at time t.
+      AD<double> x0 = vars[x_start + t - 1];
+      AD<double> y0 = vars[y_start + t - 1];
+      AD<double> psi0 = vars[psi_start + t - 1];
+      AD<double> v0 = vars[v_start + t - 1];
+      AD<double> cte0 = vars[cte_start + t - 1];
+      AD<double> epsi0 = vars[epsi_start + t - 1];
+
+      // Only consider the actuation at time t.
+      AD<double> delta0 = vars[delta_start + t - 1];
+      AD<double> a0 = vars[a_start + t - 1];
+
+      AD<double> delta;
+      AD<double> a;
+
+      switch(t) {
+        case 1:
+          delta = delta0;
+          a = a0;
+          break;
+        default:
+          //latency is 100ms, dt is also 100ms. take the previous actuation value
+          //to compensate
+          delta = vars[delta_start + t - 2];
+          a = vars[a_start + t - 2];
+          break;
+      }
+
+      AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2); //+ coeffs[3] * CppAD::pow(x0, 3);
+      AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0); //+ 3 * coeffs[3] * CppAD::pow(x0, 2));
+
+      // Recall the equations for the model:
+      // x_[t] = x[t-1] + v[t-1] * cos(psi[t-1]) * dt
+      // y_[t] = y[t-1] + v[t-1] * sin(psi[t-1]) * dt
+      // psi_[t] = psi[t-1] + v[t-1] / Lf * delta[t-1] * dt
+      // v_[t] = v[t-1] + a[t-1] * dt
+      // cte[t] = f(x[t-1]) - y[t-1] + v[t-1] * sin(epsi[t-1]) * dt
+      // epsi[t] = psi[t] - psides[t-1] + v[t-1] * delta[t-1] / Lf * dt
+      fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+      fg[1 + psi_start + t] = psi1 - (psi0 - v0 * delta / Lf * dt);
+      fg[1 + v_start + t] = v1 - (v0 + a * dt);
+      fg[1 + cte_start + t] =
+          cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+      fg[1 + epsi_start + t] =
+          epsi1 - ((psi0 - psides0) - v0 * delta0 / Lf * dt);
+
+      //Calculate cost
+      unsigned int i = t - 1;
+
+      fg[0] += 200*CppAD::pow(vars[cte_start + i], 2);
+      fg[0] += 500*CppAD::pow(vars[epsi_start + i], 2);
+      fg[0] += 100*CppAD::pow(vars[v_start + i] - ref_v, 2);
+
+      if (i < N-1){
+        //fg[0] += 100*CppAD::pow(vars[delta_start + i], 2);
+
+        //penalize harsh steering
+        if (CppAD::abs(vars[delta_start + i])/25 > 0.05){
+          fg[0] += 3000*CppAD::pow(vars[delta_start + i], 2);
+        }
+
+        if (vars[a_start + i] > 0) {
+          fg[0] += CppAD::abs(vars[v_start + i] - ref_v);
+        } else {
+          //penalize breaking for a smooth ride
+          fg[0] += 500*CppAD::pow(vars[a_start + i], 2);
+        }
+
+        // avoid steering at high speeds
+        fg[0] += 1000*CppAD::pow(vars[delta_start + i], 2) * CppAD::pow(vars[v_start+i], 2);
+
+        fg[0] += 7000*CppAD::abs(coeffs[2]) * CppAD::pow(vars[v_start+i], 2);
+      }
+
+      if (i<N-2){
+        fg[0] += 100*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+        fg[0] += 100*CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
+      }
+
+      //Add error of destination to the cost
+      //fg[0] += 100 * CppAD::pow(vars[psi_start - 1] - f0, 2);
+    }
+  }
+};
+
+//
+// MPC class definition implementation.
+//
+MPC::MPC() {}
+MPC::~MPC() {}
+
+vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
+  bool ok = true;
+  //size_t i;
+  typedef CPPAD_TESTVECTOR(double) Dvector;
+
+  size_t n_vars = 6 * N + 2 * (N - 1);
+  size_t n_constraints = 6 * N;
+
+  double x = state[0];
+  double y = state[1];
+  double psi = state[2];
+  double v = state[3];
+  double cte = state[4];
+  double epsi = state[5];
+
+  // Initial value of the independent variables.
+  // SHOULD BE 0 besides initial state.
+  Dvector vars(n_vars);
+  for (unsigned int i = 0; i < n_vars; i++) {
+    vars[i] = 0;
+  }
+
+  Dvector vars_lowerbound(n_vars);
+  Dvector vars_upperbound(n_vars);
+
+  vars[x_start] = x;
+  vars[y_start] = y;
+  vars[psi_start] = psi;
+  vars[v_start] = v;
+  vars[cte_start] = cte;
+  vars[epsi_start] = epsi;
+
+  // Set all non-actuators upper and lowerlimits
+  // to the max negative and positive values.
+  for (unsigned int i = 0; i < delta_start; i++) {
+    vars_lowerbound[i] = -1.0e19;
+    vars_upperbound[i] = 1.0e19;
+  }
+  //between 25 and -25
+  for (unsigned int i = delta_start; i < a_start; i++) {
+      vars_lowerbound[i] = -0.436332;
+      vars_upperbound[i] = 0.436332;
+  }
+
+  for (unsigned int i = a_start; i < n_vars; i++) {
+   vars_lowerbound[i] = -1.0;
+   vars_upperbound[i] = 1.0;
+ }
+  // Lower and upper limits for the constraints
+  // Should be 0 besides initial state.
+  Dvector constraints_lowerbound(n_constraints);
+  Dvector constraints_upperbound(n_constraints);
+  for (unsigned int i = 0; i < n_constraints; i++) {
+    constraints_lowerbound[i] = 0;
+    constraints_upperbound[i] = 0;
+  }
+
+  constraints_lowerbound[x_start] = x;
+  constraints_lowerbound[y_start] = y;
+  constraints_lowerbound[psi_start] = psi;
+  constraints_lowerbound[v_start] = v;
+  constraints_lowerbound[cte_start] = cte;
+  constraints_lowerbound[epsi_start] = epsi;
+
+  constraints_upperbound[x_start] = x;
+  constraints_upperbound[y_start] = y;
+  constraints_upperbound[psi_start] = psi;
+  constraints_upperbound[v_start] = v;
+  constraints_upperbound[cte_start] = cte;
+  constraints_upperbound[epsi_start] = epsi;
+
+  // object that computes objective and constraints
+  FG_eval fg_eval(coeffs);
+
+  //
+  // NOTE: You don't have to worry about these options
+  //
+  // options for IPOPT solver
+  std::string options;
+  // Uncomment this if you'd like more print information
+  options += "Integer print_level  0\n";
+  // NOTE: Setting sparse to true allows the solver to take advantage
+  // of sparse routines, this makes the computation MUCH FASTER. If you
+  // can uncomment 1 of these and see if it makes a difference or not but
+  // if you uncomment both the computation time should go up in orders of
+  // magnitude.
+  options += "Sparse  true        forward\n";
+  options += "Sparse  true        reverse\n";
+  // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
+  // Change this as you see fit.
+  options += "Numeric max_cpu_time          0.5\n";
+
+  // place to return solution
+  CppAD::ipopt::solve_result<Dvector> solution;
+
+  // solve the problem
+  CppAD::ipopt::solve<Dvector, FG_eval>(
+      options, vars, vars_lowerbound, vars_upperbound, constraints_lowerbound,
+      constraints_upperbound, fg_eval, solution);
+
+  // Check some of the solution values
+  ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+
+  // Cost
+  auto cost = solution.obj_value;
+  std::cout << "Cost " << cost << std::endl;
+
+  //cout << "Solution" << solution.x << std::endl;
+  vector<double> sol_vec;
+
+  sol_vec.push_back(solution.x[a_start]);
+  sol_vec.push_back(solution.x[delta_start]);
+
+  for (unsigned int i = 0; i < N-1; i++) {
+    sol_vec.push_back(solution.x[x_start + i + 1]);
+  }
+
+  for (unsigned int i = 0; i < N-1; i++) {
+    sol_vec.push_back(solution.x[y_start + i + 1]);
+  }
+
+  return sol_vec;
+  //return {solution.x[delta_start], solution.x[a_start]};
+}
